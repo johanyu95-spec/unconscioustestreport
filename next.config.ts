887fde9161e0bae9a0d7514d54c874546f34d8d1
hyeonwuckyou <johanyu95@gmail.com>
2025-12-24 09:50:43 +0900

    버셀 배포 에러 해결: Scheduler 모듈 경로 오류 수정 (Webpack Alias 추가)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   /* reactCompiler: true, */
   transpilePackages: ["@react-pdf/renderer"],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    config.resolve.alias.scheduler = require.resolve("scheduler");
+    return config;
+  },
 };
 
 export default nextConfig;
